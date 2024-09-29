72eef51ae37aae33aea361214b37b51b8edb71a3
Daniel Marschall <info@daniel-marschall.de>
2024-09-29 10:05:48 +0000

    Variant records
    git-svn-id: https://svn.viathinksoft.com/svn/cmdb2/trunk@30 65e16d04-c8d4-4087-98a5-90cfe6114a59

--- CmDbPluginShare.pas
+++ CmDbPluginShare.pas
@@ -9,20 +9,27 @@ type
   TCmDbPluginClickResponseAction = (craNone, craObject, craStatistics);
 
 type
-  TCmDbPluginClickResponse = packed record
-    Handled: boolean;
-    Action: TCmDbPluginClickResponseAction;
-    // Normal object, for Action=CraObject
-    ObjTable: string[100];
-    ObjId: TGuid;
-    // Statistics, for Action=craStatistics
-    StatId: TGuid;
-    StatName: string[100];
-    SqlTable: string[100];
-    SqlInitialOrder: string[250];
-    SqlAdditionalFilter: string[250];
-    BaseTableDelete: string[100];
-    Reserved: array[0..4*1024-1] of AnsiChar;
+  // TODO: These shortstrings are not Unicode capable. What to do?
+  TCmDbPluginClickResponse = packed record              // Variant Record
+    Handled: Boolean;                                   // 1 byte (packed)
+    case Action: TCmDbPluginClickResponseAction of      // 1 byte (packed)
+      craNone: (                                        // --- Variant 1
+        ReservedNone: array[0..4093] of AnsiChar;       // 4094 bytes to fill up to 4096
+      );
+      craObject: (                                      // --- Variant 2
+        ObjTable: string[100];                          // 101 bytes (100 chars + 1 byte for length)
+        ObjId: TGuid;                                   // 16 bytes
+        ReservedObject: array[0..3976] of AnsiChar;     // 3977 bytes to fill up to 4096
+      );
+      craStatistics: (                                  // --- Variant 3
+        StatId: TGuid;                                  // 16 bytes
+        StatName: string[100];                          // 101 bytes (100 chars + 1 byte for length)
+        SqlTable: string[100];                          // 101 bytes (100 chars + 1 byte for length)
+        SqlInitialOrder: string[250];                   // 251 bytes (250 chars + 1 byte for length)
+        SqlAdditionalFilter: string[250];               // 251 bytes (250 chars + 1 byte for length)
+        BaseTableDelete: string[100];                   // 101 bytes (100 chars + 1 byte for length)
+        ReservedStatistics: array[0..3272] of AnsiChar; // 3273 bytes to fill up to 4096
+      );
   end;
   PCmDbPluginClickResponse = ^TCmDbPluginClickResponse;
 
